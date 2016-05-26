53f727f78d572f823afa437713da41df991cd91f
dimaswift <dimaswift@gmail.com>
2016-05-26 10:06:26 +0300

    Raw version

---
+++ Editor/UVUnwrap.cs
@@ -0,0 +1,227 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class UVUnwrap : EditorWindow
+{
+    MeshFilter target;
+    Texture2D dummyTexture, blackTexture;
+    UVUnwrapSettings.Side draggedSide;
+    Vector2 pressedMousePos, pressedSidePos;
+    Texture2D _lockIcon;
+    Texture2D lockIcon
+    {
+        get
+        {
+            if (_lockIcon == null)
+                _lockIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/UV Unwrapper/lock.png");
+            return _lockIcon;
+        }
+    }
+
+    [MenuItem("Tools/UV Unwrap")]
+    static void Open()
+    {
+        var w = GetWindow<UVUnwrap>("UV Unwrap", true);
+
+        w.Show();
+        EditorInterface.CenterOnMainWin(w); 
+    }
+
+    void OnDestroy()
+    {
+        DestroyImmediate(dummyTexture);
+        DestroyImmediate(blackTexture);
+    }
+
+    Texture2D GetDummyTexture() 
+    {
+        if (dummyTexture == null)
+        {
+            dummyTexture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+            dummyTexture.SetPixel(0, 0, new Color(0,0,0,0));
+            dummyTexture.Apply();
+        }
+        return dummyTexture;
+    }
+
+    Texture2D GetBlackTexture()
+    {
+        if (blackTexture == null)
+        {
+            blackTexture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+            blackTexture.SetPixel(0, 0, new Color(0,0,0,.25f));
+            blackTexture.Apply();
+        }
+        return blackTexture;
+    }
+
+    void OnGUI()
+    {
+        var settings = UVUnwrapSettings.Instance;
+        if (dummyTexture == null)
+             dummyTexture = GetDummyTexture();
+        if (blackTexture == null)
+            blackTexture = GetBlackTexture();
+        var width = EditorGUILayout.GetControlRect().width;
+        var height = EditorGUILayout.GetControlRect().height;
+        settings.textureRect = settings.targetTexture == null ?
+            new Rect(5, 35, settings.textureSize * settings.gridSize, settings.textureSize * settings.gridSize) :
+            new Rect(5, 35, settings.targetTexture.width * settings.zoom, settings.targetTexture.height * settings.zoom);
+
+
+        var y = 5f;
+        float dockWidth = 350;
+        float elementYOffset = 25;
+        target = EditorGUI.ObjectField(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), target, typeof(MeshFilter), true) as MeshFilter;
+        var _sidesScale = settings.sidesScale;
+        settings.sidesScale = EditorGUI.Slider(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Sides Scale", settings.sidesScale, 0f, 1f);
+        if (settings.sidesScale != _sidesScale)
+            settings.ScaleSides();
+
+        var _pixelCount = settings.textureSize;
+        settings.textureSize = EditorGUI.IntSlider(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Pixel Count", settings.textureSize, 8, 128);
+        if (settings.textureSize != _pixelCount)
+            settings.RecalculateGrid();
+
+        var _gridSize = settings.gridSize;
+        settings.gridSize = EditorGUI.Slider(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Grid Size", settings.gridSize, 5f, 50);
+        if (settings.gridSize != _gridSize)
+            settings.RecalculateGrid();
+
+        settings.scaleSidesWithGrid = EditorGUI.Toggle(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Keep Sides Ratio", settings.scaleSidesWithGrid);
+
+        settings.snapToGrid = EditorGUI.Toggle(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Snap To Grid", settings.snapToGrid);
+        
+        settings.targetTexture = EditorGUI.ObjectField(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Target Texture", settings.targetTexture, typeof(Texture2D),  true) as Texture2D;
+
+        var _zoom = settings.zoom;
+        settings.zoom = EditorGUI.Slider(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Zoom", settings.zoom, 0.1f, 1f);
+        if(settings.zoom != _zoom)
+            settings.ScaleSides();
+
+        var gc = GUI.color;
+        y += elementYOffset;
+        for (int i = 0; i < 6; i++)
+        {
+            var side = settings.sides[i];
+            GUI.color = gc;
+            side.locked = EditorGUI.ToggleLeft(new Rect(width - dockWidth + 180, y, 50, 16), " Lock", side.locked);
+            GUI.color = side.color;
+            GUI.Box(new Rect(width - dockWidth, y, 70, 20), blackTexture);
+            GUI.color = side.locked ? gc.SetAlpha(.2f) : gc;
+            EditorGUI.LabelField(new Rect(width - dockWidth, y, 50, 16), side.name);
+            EditorGUI.LabelField(new Rect(width - dockWidth + 75, y, 150, 16), string.Format("x:{0:0.000}, y:{1:0.000}", side.uv.x, side.uv.y));
+         
+          
+        
+            y += elementYOffset;
+        }
+        GUI.color = gc;
+        var e = Event.current;
+
+        if(settings.targetTexture != null)
+        {
+            GUI.DrawTexture(settings.textureRect, settings.targetTexture);
+        }
+    
+        if (GUI.Button(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Recalculate Grid"))
+        {
+            settings.RecalculateGrid();
+        }
+
+        if (target != null)
+        {
+            if (GUI.Button(new Rect(width - dockWidth, y += elementYOffset, dockWidth, 16), "Generate Sides"))
+            {
+                settings.GenerateSides(target.transform.localScale);
+                settings.ScaleSides();
+            }
+        }
+
+        if(draggedSide != null)
+        {
+
+        }
+
+        var container = settings.textureRect;
+        if(e.button == 0)
+        {
+            if (e.type == EventType.MouseDown)
+            {
+                pressedMousePos = e.mousePosition;
+                bool touchedSide = false;
+                foreach (var side in settings.sides)
+                {
+                    if (!side.locked && side.Contains(pressedMousePos))
+                    {
+                        touchedSide = true;
+                        draggedSide = side;
+                        pressedSidePos = side.rect.position;
+                        break;
+                    }
+                }
+                if (!touchedSide)
+                    draggedSide = null;
+            }
+            if (e.type == EventType.MouseDrag && draggedSide != null)
+            {
+                var r = draggedSide.rect;
+                var pos = pressedSidePos + (e.mousePosition - pressedMousePos);
+                pos.x = Mathf.Clamp(pos.x, container.x, container.x + container.width - r.width);
+                pos.y = Mathf.Clamp(pos.y, container.y, container.y + container.height - r.height);
+                r.position = settings.snapToGrid ? settings.GetGridPoint(pos) : pos;
+                draggedSide.rect = r;
+                draggedSide.MapPositionFromRect(container);
+            }
+        }
+        if(draggedSide != null)
+        {
+            GUI.color = new Color(0, 0, 0, 1);
+            DrawRectagle(draggedSide.rect, 2);
+            GUI.color = gc;
+        }
+        if(settings.snapToGrid)
+        {
+            for (int i = 0; i < settings.grid.Width; i++)
+            {
+                GUI.DrawTexture(new Rect(container.x, settings.grid.GetColumn(i), container.width, 1), blackTexture, ScaleMode.StretchToFill);
+            }
+            for (int i = 0; i < settings.grid.Height; i++)
+            {
+                GUI.DrawTexture(new Rect(settings.grid.GetRow(i), container.y, 1, container.height), blackTexture, ScaleMode.StretchToFill);
+            }
+
+            GUI.DrawTexture(new Rect(container.x, container.y + container.height, container.width, 1), blackTexture, ScaleMode.StretchToFill);
+            GUI.DrawTexture(new Rect(container.x + container.width, container.y, 1, container.height), blackTexture, ScaleMode.StretchToFill);
+        }
+        else
+        {
+            DrawRectagle(container);
+        }
+        
+        for (int i = 0; i < settings.sides.Count; i++)
+        {
+            var side = settings.sides[i];
+
+            GUI.color = side.color;
+            GUI.Box(side.rect, dummyTexture);
+        }
+
+        Repaint();
+    }
+
+    public void DrawRectagle(Rect rect, float width = 1f)
+    {
+        GUI.DrawTexture(new Rect(rect.x - width, rect.y + rect.height, rect.width + width * 2, width), blackTexture, ScaleMode.StretchToFill);
+        GUI.DrawTexture(new Rect(rect.x + rect.width, rect.y, width, rect.height), blackTexture, ScaleMode.StretchToFill);
+        GUI.DrawTexture(new Rect(rect.x - width, rect.y - width, rect.width + width * 2, width), blackTexture, ScaleMode.StretchToFill);
+        GUI.DrawTexture(new Rect(rect.x - width, rect.y, width, rect.height), blackTexture, ScaleMode.StretchToFill);
+    }
+
+    class SidePanel
+    {
+        Color color;
+        int index;
+    }
+}
---
+++ Editor/UVUnwrapSettings.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class UVUnwrapSettings : EditorSettings<UVUnwrapSettings>
+{
+    public int textureSize = 16;
+    public int textureWidth = 16;
+    public int textureHeight = 16;
+    public float gridSize = 16;
+    [Range(0.0001f, 1f)]
+    public float sidesScale = 1f;
+    public Vector3 boxSize = new Vector3(1,1,1);
+    public List<Side> sides;
+    public bool powerOfTwo = true;
+    public bool snapToGrid = false;
+    public float zoom = 1f;
+    public Texture2D targetTexture = null;
+    [SerializeField]
+    Grid _grid;
+    public bool scaleSidesWithGrid;
+    public Grid grid { get { if (_grid == null) _grid = CreateInstance<Grid>(); return _grid; } }
+    public Rect textureRect;
+
+
+    public Point TextureSize
+    {
+        get { return powerOfTwo ? new Point(textureSize, textureSize) : new Point(textureWidth, textureHeight); }
+    }
+
+    public struct Point { public int x; public int y; public Point(int x, int y) { this.x = x; this.y = y; } }
+
+    public void RecalculateGrid()
+    {
+        grid.CreateGrid(powerOfTwo ? textureSize : textureWidth, powerOfTwo ? textureSize : textureHeight, gridSize, textureRect.position);
+        if(scaleSidesWithGrid)
+        {
+            BindSidesToContainer();
+        }
+    }
+
+    public void BindSidesToContainer()
+    {
+        foreach (var s in sides)
+        {
+            s.MapRectToContainer(textureRect);
+        }
+    }
+
+    public Texture2D GenerateTexture()
+    {
+        var t = new Texture2D(TextureSize.x, TextureSize.y, TextureFormat.ARGB32, true);
+        return t;
+    }
+
+    public Vector2[] GenerateUV()
+    {
+        var uv = new Vector2[24];
+        int unIndex = 0;
+        foreach (var side in sides)
+        {
+            uv[unIndex] = new Vector2(side.uv.x, side.uv.y);
+            uv[unIndex + 1] = new Vector2(side.uv.x + side.scaledSize.x, side.uv.y);
+            uv[unIndex + 2] = new Vector2(side.uv.x, side.uv.y + side.scaledSize.y);
+            uv[unIndex + 3] = new Vector2(side.uv.x + side.scaledSize.x, side.uv.y + side.scaledSize.y);
+            unIndex += 4;
+        }
+        return uv;
+    }
+
+    public void ScaleSides()
+    {
+        foreach (var s in sides)
+        {
+            s.scaledSize = s.size * sidesScale;
+            s.MapRectToContainer(textureRect);
+        }
+    }
+
+    public Vector2 GetGridPoint(Vector2 point)
+    {
+        return grid.GetPoint(point);
+    }
+
+    public List<Side> GenerateSides(Vector3 size)
+    {
+        boxSize = size;
+        sides = GetSides(size, 1);
+        return sides;
+    }
+    [System.Serializable]
+    public class Side
+    {
+        public string name = "side";
+        public Color color;
+        public Vector3 uv;
+        public Vector3 size;
+        public Vector3 scaledSize;
+        public bool locked = false;
+        public Rect rect;
+
+
+        public bool Contains(Vector2 point)
+        {
+            return (point.x >= rect.x &&
+                        point.x <= rect.x + rect.width &&
+                         point.y >= rect.y &&
+                          point.y <= rect.y + rect.height);
+        }
+
+        public Side(float x, float y, Color color)
+        {
+            size = new Vector3(x, y, 0.1f);
+            uv = new Vector3();
+            color.a = .35f;
+            this.color = color;
+            scaledSize = size;
+        }
+
+        public void MapRectToContainer(Rect container)
+        {
+            rect = new Rect(
+                InterfaceUtilily.Remap(uv.x, 0f, 1f, container.x, container.x + container.width),
+                InterfaceUtilily.Remap(uv.y, 1f, 0f, container.y, container.y + container.height),
+                InterfaceUtilily.Remap(scaledSize.x, 0f, 1f, 0, container.width),
+                InterfaceUtilily.Remap(scaledSize.y, 0f, 1f, 0, container.height)
+            );
+        }
+
+        public void MapPositionFromRect(Rect container)
+        {
+            uv = new Vector3(
+                InterfaceUtilily.Remap(rect.x, container.x, container.x + container.width, 0f, 1f),
+                InterfaceUtilily.Remap(rect.y, container.y, container.y + container.height, 1f, 0f)
+            );
+        }
+
+
+        public Side() { }
+
+        public static Side operator * (Side side, float s)
+        {
+            side.size *= s;
+            return side;
+        }
+    }
+
+
+    public static List<Side> GetSides(Vector3 scale, float maxSize, float size = 1)
+    {
+        var sides = new List<Side>();
+
+        sides.Add(new Side(scale.x, scale.z, Color.HSVToRGB(.1f, .7f, .7f)) * size);
+        sides.Add(new Side(scale.y, scale.z, Color.HSVToRGB(.2f, .7f, .7f)) * size);
+        sides.Add(new Side(scale.x, scale.y, Color.HSVToRGB(.3f, .7f, .7f)) * size);
+        sides.Add(new Side(scale.x, scale.z, Color.HSVToRGB(.4f, .7f, .7f)) * size);
+        sides.Add(new Side(scale.y, scale.z, Color.HSVToRGB(.5f, .7f, .7f)) * size);
+        sides.Add(new Side(scale.x, scale.y, Color.HSVToRGB(.6f, .7f, .7f)) * size);
+
+
+        sides[0].uv = Vector2.zero;
+
+        sides.Sort((s1, s2) => s1.size.magnitude.CompareTo(s2.size.magnitude));
+        var largest = sides.LastItem().size;
+        maxSize = largest.x > largest.y ? largest.x : largest.y;
+
+        foreach (var side  in sides)
+        {
+            side.size = new Vector3(InterfaceUtilily.Remap(side.size.x, 0, maxSize, 0, 1), InterfaceUtilily.Remap(side.size.y, 0, maxSize, 0, 1));
+        }
+
+        return sides;
+    }
+}
